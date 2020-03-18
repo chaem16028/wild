c61bc9dbabb6577322d33a25e1810b71a3ec1af0
chaem16028 <chaem16028@gmail.com>
2020-03-18 19:16:22 +0100

    création des fichiers footer et header

--- carreer.php
+++ carreer.php
@@ -9,27 +9,8 @@
 <body>
 
 
-     <!-- debut nav -->
-     <div class="bordernav">
-        <nav>
-            <div class="liensnav">
-                <ul class="ulnav">
-                    <div class="divlogoburger" div>
-                        <img class="logoburger" src="https://css-tricks.com/wp-content/uploads/2012/10/threelines.png" alt="menu burger">
-                    </div>
-                    <li class="linav">Techdegree</li>
-                    <li class="linav">Teams</li>
-                    <li class="linav"><a class="signin" href="sign_in.php">Sign In</a></li>
-                    <li class="linav2">Free Trial</li>
-                </ul>
-            </div>
-        </nav>
-        <div class="logo">
-            <img class="logonav" src="https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/13069487021556105336-512.png" alt="logo">
-            <p class="titre">treehouse</p>
-        </div>
-    </div>
-
+    <!-- debut nav -->
+    <?php require ("headerpage.php") ?>
 
 
 
@@ -115,38 +96,12 @@
         <p class="perks">Sorry, we have no open positions at this time.</p>
     </section>
 
-    <footer>
-        <div>
-            <img class="img" src="https://www.netclipart.com/pp/m/198-1985949_treehouse-logo-treehouse.png" alt="logo Treehouse">
-            <h1>© 2020 Treehouse Island, Inc.</h1>
-        </div>
-        <div class="menu">
-            <div class="menu1">
-                <ol>
-                    <li> <a href="about.php">About</a></li>
-                    <li><a href="carreer.php">Careers</a></li>
-                    <li> <a href="https://teamtreehouse.com/terms">Terms</a></li>
-                    <li><a href="https://teamtreehouse.com/privacy">Privacy</a></li>
-                </ol>
-            </div>
-            <div class="menu2">
-                <ol>
-                    <li><a href="https://teamtreehouse.com/support">Contact</a></li>
-                    <li> <a href="https://blog.teamtreehouse.com/?_ga=2.52710312.1268167978.1583750454-2082522592.1583750454">Blog</a></li>
-                    <li> <a href="Affiliate">Affiliate Program</a></li>
-                    <li> <a href="https://join.teamtreehouse.com/teams/?_ga=2.240603011.1268167978.1583750454-2082522592.1583750454">TalentPath</a></li>
-                </ol>
-            </div>
-        </div>
-        <div class="containerlogo">
-            <img class="Jslogo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSOB8_KJHOHX30mv6W_CDnzyt7KaqcNyZ5XuCP6WWM5F9KbqHRg" alt="Twitter">
-            <img class="Jslogo" src="https://images.squarespace-cdn.com/content/v1/58cb4cb3440243037fbb8cb0/1547668767418-J06NKVSNGJWZTGQUNKJ3/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFUQAah1E2d0qOFNma4CJuw0VgyloEfPuSsyFRoaaKT76QvevUbj177dmcMs1F0H-0/Logo+youtube+noir.png"
-                alt="logo youtube">
-            <img class="Jslogo" src="https://i.pinimg.com/originals/c5/ef/04/c5ef04b0aeb3c8d4e929360ec9709602.png" alt="logo facebook">
-            <img class="Jslogo" src="https://cdn.icon-icons.com/icons2/1558/PNG/512/353425-linkedin-logo_107480.png" alt="logo linkedin">
-            <img class="Jslogo" src="https://cdn.pixabay.com/photo/2016/08/01/21/02/icon-1562139_960_720.png" alt="logo instagram">
-        </div>
-    </footer>
+
+
+    <!-- debut footer -->
+    <?php require ("footerpage.php") ?>
+
+
 
 </body>
 </html>
---
+++ headerpage.php
@@ -0,0 +1,20 @@
+ <!-- debut nav -->
+ <div class="bordernav">
+        <nav>
+            <div class="liensnav">
+                <ul class="ulnav">
+                    <div class="divlogoburger" div>
+                        <img class="logoburger" src="https://css-tricks.com/wp-content/uploads/2012/10/threelines.png" alt="menu burger">
+                    </div>
+                    <li class="linav">Techdegree</li>
+                    <li class="linav">Teams</li>
+                    <li class="linav"><a class="signin" href="sign_in.php">Sign In</a></li>
+                    <li class="linav2">Free Trial</li>
+                </ul>
+            </div>
+        </nav>
+        <div class="logo">
+            <img class="logonav" src="https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/13069487021556105336-512.png" alt="logo">
+            <p class="titre">treehouse</p>
+        </div>
+    </div>
